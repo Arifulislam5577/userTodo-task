71698b15bb0d6875fcfe89e175d38434331c5f78
Arif5577 <arifalamm33@gmail.com>
2022-12-10 10:56:25 +0600

    get todos from database

--- index.js
+++ index.js
@@ -2,41 +2,26 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-import userRouter from "./routes/userRouter.js";
-import todoRouter from "./routes/todoRouter.js";
-const __filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(__filename);
+import userRouter from "./apiRoutes/userRouter.js";
+import todoRouter from "./apiRoutes/todoRouter.js";
+import rootRoutes from "./routes/rootRouter.js";
+
 dotenv.config();
 const app = express();
 
+// DEFAULT MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 mongoose.set("strictQuery", true);
 
 // VIEW ENGINE
 app.set("view engine", "ejs");
-app.set("/css", path.resolve(dirname, "/public/css"));
 
-// ROUTES
+// CLIENT SIDE ROUTES
+app.use("/", rootRoutes);
 
-app.get("/", (req, res) => {
-  res.render("index", { pageTitle: "User Todo || Save Your Data" });
-});
-
-app.get("/signin", (req, res) => {
-  res.render("SignIn", { pageTitle: "Login || Save Your Data" });
-});
-
-app.get("/signup", (req, res) => {
-  res.render("SignUp", { pageTitle: "Create User || Save Your Data" });
-});
-
-app.get("/adduser", (req, res) => {
-  res.render("AddUser", { pageTitle: "Add New User|| Save Your Data" });
-});
-
+// API ROUTES
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/todo", todoRouter);
 
---
+++ routes/rootRouter.js
@@ -0,0 +1,29 @@
+import express from "express";
+import axios from "axios";
+const rootRoutes = express.Router();
+
+rootRoutes.get("/", async (req, res) => {
+  try {
+    const { data } = await axios.get("http://localhost:5000/api/v1/todo");
+    res.render("index", {
+      pageTitle: "User Todo || Save Your Data",
+      todos: data,
+    });
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+rootRoutes.get("/signin", (req, res) => {
+  res.render("SignIn", { pageTitle: "Login || Save Your Data" });
+});
+
+rootRoutes.get("/signup", (req, res) => {
+  res.render("SignUp", { pageTitle: "Create User || Save Your Data" });
+});
+
+rootRoutes.get("/adduser", (req, res) => {
+  res.render("AddUser", { pageTitle: "Add New User|| Save Your Data" });
+});
+
+export default rootRoutes;
